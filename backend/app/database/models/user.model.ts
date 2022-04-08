340096c6a31c7f0a60a355e21cfba4c6e930a4be
Przemysław Gajowczyk <przemyslaw.gajowczyk23@gmail.com>
2022-04-09 00:40:55 +0200

    Add ad interface and model

--- backend/app/database/models/user.model.ts
+++ backend/app/database/models/user.model.ts
@@ -2,14 +2,13 @@ import {Schema, model} from 'mongoose';
 import User from './user.interface'
 
 const userSchema = new Schema<User>({
-    userId: { type: String, required: true},
     firstName: { type: String, required: true},
     lastName: { type: String, required: true},
     email: { type: String, required: true},
     password: { type: String, required: true},
     phone: { type: String, required: true},
     city: { type: String, required: true},
-    userRole: { type: String, required: true},
+    role: { type: String, required: true},
     verifiedByEmail: { type: Boolean },
     verifiedByAdmin: { type: Boolean }
 })
