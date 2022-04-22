e13d52194e2dcb43dae48d2edf66e6b4ffd6e013
KrisGum <krisgumularz@gmail.com>
2022-04-22 18:56:00 +0200

    Nodemailer test

--- backend/app/services/nodemailer.ts
+++ backend/app/services/nodemailer.ts
@@ -1,17 +1,25 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 
+
+
 export let transporter = nodemailer.createTransport({
-    host: 'smpt.gmail.com',
+    host: "smtp.gmail.com",
     port: 465,
-    secure: false,
+    secure: true,
     auth: {
-        type: '0Auth2',
-        user: process.env.MAIL_USERNAME,
-        pass: process.env.MAIL_PASSWORD,
-        clientId: 
+        type: "OAuth2",
+        user: process.env.MAIL_USER,
+        
+    },
+});
+
+transporter.set("oauth2_provision_cv", (user: string, renew: boolean, callback:string) => {
+    let accessToken = userTokens[user];
+    if(!accessToken) {
+        return callback(new Error("Unknown user"));
+    } else {
+        return callback(null, accessToken);
     }
 
-
-
-})
+});
