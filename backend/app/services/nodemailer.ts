57e30a81c00ce8dab8f29dae6d32b1f780945064
KrisGum <krisgumularz@gmail.com>
2022-04-24 12:22:35 +0200

    Fixing nodemailer and jwt

--- backend/app/services/nodemailer.ts
+++ backend/app/services/nodemailer.ts
@@ -1,20 +1,16 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 
-
-
 export let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-       user: process.env.MAIL_USER,
-       pass: process.env.MAIL_PASSWORD 
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
+  service: 'gmail',
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
-
-
 
 // transporter.set("oauth2_provision_cv", (user: string, renew: boolean, callback:string) => {
 //     let accessToken = userTokens[user];
