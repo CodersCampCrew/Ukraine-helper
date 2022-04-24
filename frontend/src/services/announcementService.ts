5ff29637006c69f54f973f11686e8dc1d4fe5a7b
DawidZlobecki <96154754+DawidZlobecki@users.noreply.github.com>
2022-04-25 00:38:41 +0200

    Uh 42  (#22)
    * fix: remove redundant routes
    
    * feat: added announcements list
    
    * fix: some fixes

--- frontend/src/services/announcementService.ts
+++ frontend/src/services/announcementService.ts
@@ -4,7 +4,13 @@ const announcementService = {
     const data = await serverAPI.get({
       url: `ad/${id}`
     });
-    console.log('asd');
+    return data;
+  },
+
+  async getAnnouncements(type: string) {
+    const data = await serverAPI.get({
+      url: `ad/${type}`
+    });
     return data;
   }
 };
