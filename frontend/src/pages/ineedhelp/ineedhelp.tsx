bd57b349d33dfaf7e374c03747fff77709ef7fb8
Dejw <dawidzlobecki2@gmail.com>
2022-04-25 13:26:52 +0200

    fix: some fixes

--- frontend/src/pages/ineedhelp/ineedhelp.tsx
+++ frontend/src/pages/ineedhelp/ineedhelp.tsx
@@ -1,20 +1,44 @@
 import { Grid, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import routes from '../../routes';
-import {NeedButton} from './components/Button'
+import { NeedButton } from './components/Button';
 
 export const IneedHelp: React.FC = () => {
-let navigate = useNavigate();
+  let navigate = useNavigate();
   return (
-    <Grid container
-    spacing={0}
-    direction="column"
-    alignItems="center"
-    justifyContent="center">
-            <NeedButton onClick={()=>{navigate(routes.announcements)}}> I Need Help </NeedButton>
-            <NeedButton onClick={()=>{navigate(routes.register)}}> I Can Help </NeedButton>
-            <div> I already have an account: </div>
-            <Button variant="contained" size="large" onClick={()=>{navigate(routes.login)}}>Log in</Button>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <NeedButton
+        onClick={() => {
+          navigate(routes.category);
+        }}
+      >
+        {' '}
+        I Need Help{' '}
+      </NeedButton>
+      <NeedButton
+        onClick={() => {
+          navigate(routes.register);
+        }}
+      >
+        {' '}
+        I Can Help{' '}
+      </NeedButton>
+      <div> I already have an account: </div>
+      <Button
+        variant="contained"
+        size="large"
+        onClick={() => {
+          navigate(routes.login);
+        }}
+      >
+        Log in
+      </Button>
     </Grid>
   );
 };
